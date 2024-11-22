354a26c52fc0f4ee38e5d5d4acd40cdffa8cefd4
Pol Gubau Amores <gubaupol@gmail.com>
2024-11-22 09:30:34 +0100

    feat: integrate Providers component into RootLayout and update Tailwind config for pol-ui plugin

--- apps/landing/tailwind.config.ts
+++ apps/landing/tailwind.config.ts
@@ -1,18 +1,19 @@
-import type { Config } from "tailwindcss";
+import { poluiPlugin } from "pol-ui"
+import type { Config } from "tailwindcss"
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/pol-ui/lib/**/*.js",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       background: "var(--background)",
+  //       foreground: "var(--foreground)",
+  //     },
+  //   },
+  // },
+  plugins: [poluiPlugin()],
+} satisfies Config
