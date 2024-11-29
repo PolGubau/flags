c5d555c6e5c1c4883b21e72f19a8fc22bc403dc8
Pol Gubau Amores <gubaupol@gmail.com>
2024-11-29 09:33:29 +0100

    Pol Gubau; MW-

--- apps/landing/src/components/Background.tsx
+++ apps/landing/src/components/Background.tsx
@@ -6,7 +6,7 @@ const Background = () => {
   return (
     <div className="max-w-[100vw] pointer-events-none overflow-hidden -z-10">
       {[...Array(25)].map((_, i) => (
-        <div key={i} className="rainbow overflow-hidden lg:rotate-12" />
+        <div key={i} className="rainbow overflow-hidden lgsd:rotate-12" />
       ))}
     </div>
   )
