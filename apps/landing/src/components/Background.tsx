d63407981e6a64416b522884856c69d7357c70b8
Pol Gubau Amores <gubaupol@gmail.com>
2024-12-27 11:11:12 +0100

    refactor: clean up code formatting and improve component structure

--- apps/landing/src/components/Background.tsx
+++ apps/landing/src/components/Background.tsx
@@ -1,11 +1,12 @@
 import "./bg.css";
 
 export const Background = () => {
-  return (
-    <div className="max-w-[100vw] pointer-events-none overflow-hidden -z-10">
-      {[...new Array(25)].map((_) => (
-        <div key={_} className="rainbow overflow-hidden lgsd:rotate-12" />
-      ))}
-    </div>
-  );
+	return (
+		<div className="max-w-[100vw] pointer-events-none overflow-hidden -z-10">
+			{[...new Array(25)].map((_, i) => (
+				// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+				<div key={i} className="rainbow overflow-hidden lgsd:rotate-12" />
+			))}
+		</div>
+	);
 };
--- apps/landing/src/components/Filters/Filters.tsx
+++ apps/landing/src/components/Filters/Filters.tsx
@@ -5,20 +5,20 @@ import { Input } from "pol-ui";
 import { TbSearch } from "react-icons/tb";
 
 export const Filters = () => {
-  const [search, setSearch] = useQueryState(
-    "q",
-    parseAsString.withOptions({ shallow: false }).withDefault(""),
-  );
+	const [search, setSearch] = useQueryState(
+		"q",
+		parseAsString.withOptions({ shallow: false }).withDefault(""),
+	);
 
-  return (
-    <div className="w-full sticky top-0 z-10 flex justify-center max-w-md">
-      <Input
-        placeholder="Search for a flag"
-        leftComponent={<TbSearch />}
-        value={search}
-        type="search"
-        onChange={(e) => setSearch(e.target.value)}
-      />
-    </div>
-  );
+	return (
+		<div className="w-full sticky top-0 z-10 flex justify-center max-w-md">
+			<Input
+				placeholder="Search for a flag"
+				leftComponent={<TbSearch />}
+				value={search}
+				type="search"
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+		</div>
+	);
 };
