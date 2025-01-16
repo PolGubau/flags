0a12cae2bdd9e4a1b04868fa0c45506b37634b58
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-16 22:22:53 +0100

    refactor(flags): ♻️ improve type imports and formatting in flag component

--- packages/flags/src/flag.tsx
+++ packages/flags/src/flag.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { FlagCode, FlagsEnum } from "./flags";
+import type React from "react";
+import { type FlagCode, FlagsEnum } from "./flags";
 
 interface FlagProps extends React.SVGProps<SVGSVGElement> {
-    code: FlagCode;
-    size?: number;
+	code: FlagCode;
+	size?: number;
 }
 export const Flag = ({ code, size, ...props }: FlagProps) => {
-    const Component = FlagsEnum[code];
-    return (
-        <Component
-            {...props}
-            width={props.width ?? size ?? undefined}
-            height={props.height ?? size / 1.4 ?? undefined}
-        />
-    );
+	const Component = FlagsEnum[code];
+	return (
+		<Component
+			{...props}
+			width={props.width ?? size ?? undefined}
+			height={props.height ?? size / 1.4 ?? undefined}
+		/>
+	);
 };
